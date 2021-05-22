d8a754284af3c87b007a6aaf104447bf2cd8dd33
Ndiaye Siré <ndiayesirekane@gmail.com>
2021-05-22 23:47:46 +0000

    Design

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image,ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image,ScrollView, Switch} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
+
+
+
 export default function App() {
+  
+
+  
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
+    
     <View style={styles.container}>
       
-      <View style={{backgroundColor:"midnightblue", height : "45%", width:"100%",justifyContent:"center", alignItems:"center"}}>
-      <Text style={{fontSize:40, color:"white", fontWeight:"900",position:'absolute',paddingBottom:120}}>16.000F</Text>
+      <View style={{backgroundColor:"midnightblue", height : "45%", width:"100%",justifyContent:"center", alignItems:"center",}}>
+      
+      <Text style={{fontSize:40, color:"white", fontWeight:"900",position:'absolute',paddingBottom:180}}>95.000F</Text>
       <AntDesign name="setting" size={25} color="white"
       style={{position:'absolute',paddingBottom:210, paddingRight:320,fontWeight:"600"}}/>
+
+    
+
+      
 
       <Text>
       </Text>
@@ -37,52 +51,34 @@ export default function App() {
       </View>
 
 
-    <View style={{flexDirection: 'row', justifyContent:"center",paddingTop:320,paddingLeft:25,position:'absolute',flex: 1,}}>
+    <View style={{flexDirection: 'row', justifyContent:"center",paddingTop:320,paddingLeft:25,position:'absolute',flex: 1,paddingLeft:375,}}>
 
-        <View style={{ height: 95, width: 110, backgroundColor: 'white',shadowColor: '#000',shadowOffset: {width: 0, height: 2},shadowOpacity: 0.2,shadowRadius: 4,borderBottomLeftRadius:15,borderTopLeftRadius:15 }}>
+        <View style={{position:'absolute',marginTop:326, height: 90, width: 322, backgroundColor: 'white',shadowColor: '#000',shadowOffset: {width: 0, height: 2},shadowOpacity: 0.2,shadowRadius: 4,borderBottomRightRadius:15,borderBottomLeftRadius:15,borderTopLeftRadius:15,borderTopRightRadius:15 }}>
         <AntDesign 
         style={{
-          paddingLeft:30,
-          paddingTop:22
+          paddingLeft:8,
+          paddingTop:24
         }}
-        name="user" size={40} color="midnightblue"/>
-        <Text style={{ color: "midnightblue",
-              paddingLeft: 35,
-              paddingTop: 9,
-              fontSize: 16,
-              fontWeight:'500',
-            }}>Payer</Text>
+        name="wifi" size={40} color="green"/>
+        <Switch
+    trackColor={{ false: "#767577", true: "green" }}
+    thumbColor={isEnabled ? "white" : "#f4f3f4"}
+    ios_backgroundColor="midnightblue"
+    onValueChange={toggleSwitch}
+    value={isEnabled}
+    style={{position:'absolute', marginLeft:250,
+    marginTop:29}}
+    />
+
+<Text style={{fontSize:16,position:'absolute',paddingLeft:70,
+          paddingTop:38,color:"midnightblue",fontWeight:"600"}}>
+          {isEnabled ? 'Vous etes en Ligne' : 'Vous etes Hors Ligne'}
+
+          
+ </Text>
+     
         </View>
-        <View style={{ height: 95, width: 110, backgroundColor: 'white',shadowColor: '#000',shadowOffset: {width: 0, height: 2},shadowOpacity: 0.2,shadowRadius: 4 }}>
-          
-        <AntDesign 
-        style={{
-          paddingLeft:30,
-          paddingTop:22
-        }}
-        name="bulb1" size={40} color="#FF8C00"/>
-        <Text style={{ color: "#FF8C00",
-              paddingLeft: 24,
-              paddingTop: 9,
-              fontSize: 16,
-              fontWeight:'500',
-            }}>Factures</Text>
-          </View>
-        <View style={{height: 95, width: 110, backgroundColor: 'white',shadowColor: '#000',shadowOffset: {width: 0, height: 2},shadowOpacity: 0.2,shadowRadius: 4,borderBottomRightRadius:15,borderTopRightRadius:15 }}>
-          
-        <AntDesign 
-        style={{
-          paddingLeft:30,
-          paddingTop:22
-        }}
-        name="mobile1" size={40} color="#87CEFA"/>
-        <Text style={{ color: "#87CEFA",
-              paddingLeft: 29,
-              paddingTop: 9,
-              fontSize: 16,
-              fontWeight:'500',
-            }}>Crédit</Text>
-          </View>
+        
 
       </View>
 
@@ -113,19 +109,20 @@ export default function App() {
               fontWeight: '600',
             }}
           >
-            Liberté 6 Place de l'indépendance
+           Dépot
           </Text>
 
           <Text
             style={{
               
               color: "midnightblue",
-              paddingLeft: 40,
+              paddingLeft: 268,
               fontSize: 16,
               fontWeight:'500',
+              position:'absolute'
             }}
           >
-            15000F
+            5000F
           </Text>
           
         </View>
@@ -164,71 +161,19 @@ export default function App() {
               fontWeight: '600',
             }}
           >
-            Liberté 6 Place de l'indépendance
+            Retrait
           </Text>
 
           <Text
             style={{
-              
               color: "midnightblue",
-              paddingLeft: 40,
+              paddingLeft: 268,
               fontSize: 16,
               fontWeight:'500',
+              position:'absolute'
             }}
           >
-            15000F
-          </Text>
-          
-        </View>
-
-        
-
-
-        <Text
-          style={{
-            fontSize: 13,
-            marginRight: -5,
-            marginVertical: 8,
-            color: "gray",
-            fontWeight: '600',
-            marginLeft:20
-          }}
-        >
-         27 Février 2021 à 14:50
-        </Text>
-
-
-        <View
-          style={{
-            flexDirection: "row",
-            marginTop: 10,
-            alignItems: "center",
-            marginLeft:20,
-            
-            
-          }}
-        >
-          <Text
-            style={{
-              
-              color: "midnightblue",
-              fontSize: 16,
-              fontWeight: '600',
-            }}
-          >
-            Liberté 6 Place de l'indépendance
-          </Text>
-
-          <Text
-            style={{
-              
-              color: "midnightblue",
-              paddingLeft: 40,
-              fontSize: 16,
-              fontWeight:'500',
-            }}
-          >
-            15000F
+            -6500F
           </Text>
           
         </View>
@@ -268,16 +213,70 @@ export default function App() {
               fontWeight: '600',
             }}
           >
-            Liberté 6 Place de l'indépendance
+            A Djibril Bathily 778474149
           </Text>
 
           <Text
             style={{
               
               color: "midnightblue",
-              paddingLeft: 40,
+              paddingLeft: 268,
               fontSize: 16,
               fontWeight:'500',
+              position:'absolute'
+            }}
+          >
+            170000F
+          </Text>
+          
+        </View>
+
+        
+
+
+        <Text
+          style={{
+            fontSize: 13,
+            marginRight: -5,
+            marginVertical: 8,
+            color: "gray",
+            fontWeight: '600',
+            marginLeft:20
+          }}
+        >
+         27 Février 2021 à 14:50
+        </Text>
+
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: 10,
+            alignItems: "center",
+            marginLeft:20,
+            
+            
+          }}
+        >
+          <Text
+            style={{
+              
+              color: "midnightblue",
+              fontSize: 16,
+              fontWeight: '600',
+            }}
+          >
+            A Ndiaye Siré Kane 778673184
+          </Text>
+
+          <Text
+            style={{
+              
+              color: "midnightblue",
+              paddingLeft: 268,
+              fontSize: 16,
+              fontWeight:'500',
+              position:'absolute'
             }}
           >
             15000F
@@ -320,19 +319,20 @@ export default function App() {
               fontWeight: '600',
             }}
           >
-            Liberté 6 Place de l'indépendance
+            Retrait
           </Text>
 
           <Text
             style={{
               
               color: "midnightblue",
-              paddingLeft: 40,
+              paddingLeft: 268,
               fontSize: 16,
               fontWeight:'500',
+              position:'absolute'
             }}
           >
-            15000F
+            -15000F
           </Text>
           
         </View>
@@ -371,19 +371,20 @@ export default function App() {
               fontWeight: '600',
             }}
           >
-            Liberté 6 Place de l'indépendance
+            Dépot
           </Text>
 
           <Text
             style={{
               
               color: "midnightblue",
-              paddingLeft: 40,
+              paddingLeft: 268,
               fontSize: 16,
               fontWeight:'500',
+              position:'absolute'
             }}
           >
-            15000F
+            4500F
           </Text>
           
         </View>
@@ -416,6 +417,7 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -442,6 +444,9 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.2,
     shadowRadius: 4,
+    position:'absolute',
+    width:350,
+    marginTop:440
   },
 
   cardBodyTop: {
